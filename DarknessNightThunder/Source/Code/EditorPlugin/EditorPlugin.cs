fb6da98716665e9fc9a03263b8cea328a8f5b11c
Adam <adam@adamslair.net>
2016-06-25 13:28:25 +0200

    Initial Install

---
+++ DarknessNightThunder/Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace DarknessNightThunder.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class DarknessNightThunderEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "DarknessNightThunderEditorPlugin"; }
+		}
+	}
+}
